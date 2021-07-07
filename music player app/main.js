4593ea7567e40fcc2e497d8e3f57105a51e903cc
shubhamjoshi99899 <83740922+shubhamjoshi99899@users.noreply.github.com>
2021-07-07 23:07:20 +0530

    Add files via upload

---
+++ music player app/main.js
@@ -0,0 +1,13 @@
+var mysong = document.getElementById("song");
+var icon = document.getElementById("icon");
+
+icon.onclick = function (){
+    if(song.paused){
+        song.play();
+        icon.src = "assets/pause.png";
+    }else{
+        song.pause();
+        icon.src = "assets/play.png";
+    }
+    
+  }
